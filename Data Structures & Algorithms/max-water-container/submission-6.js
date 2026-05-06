fed8fc35ae67b834d52f00b42ce558ad64946ada
Sachin Mehra <113981258+Sachinnmehra11@users.noreply.github.com>
2026-05-07 00:20:09 +0530

    Add: max-water-container - submission-6

---
+++ Data Structures & Algorithms/max-water-container/submission-6.js
@@ -0,0 +1,25 @@
+class Solution {
+    /**
+     * @param {number[]} heights
+     * @return {number}
+     */
+    maxArea(heights) {
+        let area=0;
+        let right=heights.length-1;
+        let left=0;
+        while(left!=right){
+            let currentArea=Math.min(heights[left],heights[right])*(right-left);
+            area=Math.max(area,currentArea);
+            if(heights[left]<heights[right]){
+                left++;
+            }
+            else{
+                right--;
+                
+            }
+            
+        }
+        return area;
+        
+    }
+}
