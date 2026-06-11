c62413aa85e5ce877f7c0714e858bbf417ad3faa
Sachin Mehra <113981258+Sachinnmehra11@users.noreply.github.com>
2026-06-11 18:42:07 +0530

    Add: serialize-and-deserialize-binary-tree - submission-2

---
+++ Data Structures & Algorithms/serialize-and-deserialize-binary-tree/submission-2.js
@@ -0,0 +1,45 @@
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     constructor(val = 0, left = null, right = null) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+
+class Codec {
+    /**
+     * Encodes a tree to a single string.
+     *
+     * @param {TreeNode} root
+     * @return {string}
+     */
+    serialize(root) {
+        if (!root) return "null";
+        let leftString = this.serialize(root.left);
+        let rightString = this.serialize(root.right);
+        return root.val + "," + leftString + "," + rightString;
+    }
+
+    /**
+     * Decodes your encoded data to tree.
+     *
+     * @param {string} data
+     * @return {TreeNode}
+     */
+    deserialize(data) {
+        let vals = data.split(',');
+        function build(){
+            let val= vals.shift();
+            if(val==="null") return null
+            let newNode = new TreeNode(parseInt(val));
+            newNode.left=build();
+            newNode.right=build();
+            
+            return newNode
+        }
+        return build();
+    }
+}
