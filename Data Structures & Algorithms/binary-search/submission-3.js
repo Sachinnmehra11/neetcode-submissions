5fb63e259bdd0b833ab3717c422402f5cb4c04bb
Sachin Mehra <113981258+Sachinnmehra11@users.noreply.github.com>
2026-08-17 22:11:32 +0530

    Add: binary-search - submission-3

---
+++ Data Structures & Algorithms/binary-search/submission-3.js
@@ -0,0 +1,24 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @param {number} target
+     * @return {number}
+     */
+    search(nums, target) {
+        let start = 0;
+        let end = nums.length - 1;
+        while (start <= end) {
+            const mid = Math.floor((start + end) / 2);
+            if (target === nums[mid]) {
+                return mid;
+            } else if (target > nums[mid]) {
+                //target is on the right side
+                start = mid + 1;
+            } else {
+                //target on left side
+                end = mid - 1;
+            }
+        }
+        return -1;
+    }
+}
